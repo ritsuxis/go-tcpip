35e1487be855d65ea96fd49737d862a82a1c8feb
ritsuxis <ritsuxis@gmail.com>
2021-09-12 00:28:43 +0900

    Improvement: formatted files in pkg/icmp

---
+++ pkg/icmp/icmp.go
@@ -0,0 +1,53 @@
+package icmp
+
+import (
+	"github.com/ritsuxis/go-tcpip/pkg/ip"
+	"github.com/ritsuxis/go-tcpip/pkg/net"
+)
+
+func init() {
+	ip.RegisterProtocol(net.ProtocolNumberICMP, rxHandler)
+}
+
+func Init() {
+	// do nothing
+}
+
+func rxHandler(iface net.ProtocolInterface, data []byte, src, dst net.ProtocolAddress) error {
+	msg, err := parse(data)
+	if err != nil {
+		return err
+	}
+	switch msg.messageType() {
+	case messageTypeEcho:
+		request := msg.(*messageEcho)
+		reply := &messageEcho{
+			header: header{messageTypeEchoReply, 0, 0},
+			id:     request.id,
+			seq:    request.seq,
+			data:   request.data,
+		}
+		return tx(iface, reply, src)
+	case messageTypeEchoReply:
+		// do nothing
+	}
+	return nil
+}
+
+func tx(iface net.ProtocolInterface, msg message, dst net.ProtocolAddress) error {
+	buf, err := msg.marshal()
+	if err != nil {
+		return err
+	}
+	return iface.Tx(net.ProtocolNumberICMP, buf, dst)
+}
+
+func EchoRequest(data []byte, dst net.ProtocolAddress) error {
+	request := &messageEcho{
+		header: header{messageTypeEcho, 0, 0},
+		id:     0,
+		seq:    0,
+		data:   data,
+	}
+	return tx(ip.GetInterfaceByRemoteAddress(dst), request, dst)
+}
